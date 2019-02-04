a09096bac267c3d9e63b39f9c73948094498704f
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2019-02-03 17:42:42 -0800

    update tuple gram

--- tuples_histograms.py
+++ tuples_histograms.py
@@ -6,38 +6,23 @@ def list_words(file):
     f.close()
     return list
 
-def remove_punc(list):
-    punctuation = ["@" , "#" , "$" , ":", ";", "_", "*" , "}" , "[" , "{" , "]" , "," , ".", "!" , "?"]
-    for i, word in enumerate(list):
-        new_word = ''
-        for char in word:
-            if char not in punctuation:
-                new_word += char
-        list[i] = new_word
-        return list
-
-
-def lower_list(list):
-    list = [x.lower() for x in list]
-    return list
-
 
 def words_tuples(list):
     list.sort()
-    list = []
+    new_list = []
     count = 0
     index = None
     for word in list:
         if word == index:
             count += 1
         else:
-            list.append((index, count))
+            new_list.append((index, count))
             index = word
             count = 1
     else:
-        list.append((index, count))
-        list.pop(0)
-    return list
+        new_list.append((index, count))
+        new_list.pop(0)
+    return new_list
 
 def unique_words_tuples(histogram):
     return len(histogram)
@@ -50,17 +35,34 @@ def frequency_tuples(histogram, word):
     else:
         return('Not found')
 
-def test_histogram():
+# def test_histogram():
+#     params = sys.argv[1:]
+#     file = params[0]
+#     words_list = list_words(file)
+#     tuple = words_tuples(words_list)
+#     print('Histogramz: {}'.format(tuple))
+
+# def get_length():
+#     params = sys.argv[1:]
+#     file = params[0]
+#     words_list = list_words(file)
+#     tuple = words_tuples(words_list)
+#     unique_keys = unique_words_tuples(tuple)
+#     print(unique_keys)
+
+def get_frequency():
     params = sys.argv[1:]
     file = params[0]
-    words_list = words_tuples(list)
-    print('Histogramz: {}'.format(words_list))
+    word = params[1]
+    words_list = list_words(file)
+    tuple = words_tuples(words_list)
+    freq = frequency_tuples(tuple, word)
+    print(freq)
 
 
 if __name__ == '__main__':
-    
-    # print('file: {}'.format(file))
-    # print((wordsDict(lowerList(remove_punc(listWords(file))))))
-    
-    # print((wordsDict(lowerList(remove_punc(listWords(file))))))
-    test_histogram()
+    # #for histogram
+    # test_histogram()
+    # #for histogram
+    # get_length()
+    get_frequency()
