5b67f497f6e633ce5e8b420ef86cb01a4515c773
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2019-02-12 13:06:37 -0800

    words.py works

--- modules/words.py
+++ modules/words.py
@@ -1,4 +1,18 @@
-def words(file_name):
-    "Open the file and make the words into a list to be used by markov"
+def words_list(file):
+    "Open the file and make the words into a list to be used by markov"    
+    if 'modules' in sys.path[0]:
+        path = 'corpuses/'
+    else:
+        path = 'modules/corpuses/'
+    with open(path + file) as file:
+        words = file.read().split()
+    return words
+
+
+if __name__ == "__main__":
+    import sys
+    words = words_list(sys.argv[1])
+    for word in words:
+        print(word)
 
     
