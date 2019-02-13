d2cea302550cc1f21595a02efde28335dcd98d1c
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2019-02-13 09:11:22 -0800

    preparing to refactor sample.py

---
+++ modules/broken_sample.py
@@ -0,0 +1,63 @@
+import sys
+import random
+
+word_list  = "one fish two fish green fish blue fish".split()
+
+"""Your first task is to write a function that takes a histogram (however you've structured yours)
+and returns a single word, at random. It should not yet take into account the distributions of 
+the words. """
+
+def one_word():
+    """Take in a string from the command line, create histogram, and return one word """
+    histo = {}
+    #make a weighted histogram based on word frequency
+    for word in word_list: 
+        if word in histo.keys():
+            histo[word] += 1
+        else:
+            histo[word] = 1
+
+    count = 0
+    random_number = random.random()
+    for key, value in histo.items():
+        count += (value /len(word_list))
+        if count >= random_number:
+            return key
+
+
+# """Once you have that working, can you prove that your code is truly random?
+#  Granted, proving randomness is much more difficult than disproving it, 
+#  but you can get close. Is the probability that any word is selected the 
+#  same as for any other word?  """
+
+# def weighted_list(histogram):
+#     """ Prove truly random """
+#     count = 0
+#     #Return the next random floating point number in the range [0.0, 1.0).
+#     random_number = random.random()
+#     #method returns a view object that displays a list of dictionary's (key, value) tuple pairs.
+#     for key, value in histogram.items():
+#         count += (value / len(params))
+#         if count >= random_number:
+#             return key
+
+def test():
+    histogram = {}
+    for i in range(0, 10000):
+        word = one_word()
+        if word in histogram:
+            histogram[word] += 1
+        else:
+            histogram[word] = 1
+    return histogram
+
+
+
+if __name__ in '__main__':
+    #part-one
+    print(one_word())
+    print(test())
+
+
+
+    
--- modules/tokenize.py
+++ modules/tokenize.py
@@ -1,18 +1,19 @@
 import sys 
 
 def words_list(file):
-    "Open the file and make the words into a list to be used by markov"    
+    "Open the file and make the words into a list to be used by markov"
+    # you need to be on the right path    
     if 'modules' in sys.path[0]:
         path = 'corpuses/'
     else:
         path = 'modules/corpuses/'
+    # use with so do not have to close
     with open(path + file) as file:
         words = file.read().split()
     return words
 
 
 if __name__ == "__main__":
-    import sys
     words = words_list(sys.argv[1])
     for word in words:
         print(word)
