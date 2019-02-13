48b00c992b1febb1ed29bf12d3251e5c065c72f2
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2019-02-13 09:25:52 -0800

    still refactoring sample.py

---
+++ modules/new_sample.py
@@ -0,0 +1,16 @@
+import sys
+import random
+
+
+def one_word():
+     """Take in a histogram, and return one word """
+    histogram_type = str(type())
+    pass
+
+
+def test():
+    pass
+
+
+if __name__ == "__main__":
+    pass
