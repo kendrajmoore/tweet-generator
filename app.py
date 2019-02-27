fbb364058422a3028f458ef9b87cc539cfc62e89
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2019-02-27 11:43:45 -0800

    module

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+# need to add bootstrap module
+# from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 # from flask import render_template
 # from app import app
